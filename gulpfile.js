a5ef6dfcc925acaf273585a9ebd76700e3d0469c
SignpostMarv <me@signpostmarv.name>
2020-04-12 12:46:52 +0100

    splitting css to separate file

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const rollup = require('rollup');
 const htmlmin = require('gulp-htmlmin');
 const newer = require('gulp-newer');
 const eslint = require('gulp-eslint');
+const postcss = require('gulp-postcss');
 
 const rollupPlugins = {
 	commonjs: require('@rollup/plugin-commonjs'),
@@ -10,6 +11,15 @@ const rollupPlugins = {
 	jsonResolve: require('@rollup/plugin-json'),
 	typescript: require('@rollup/plugin-typescript'),
 	minifyHtml: require('rollup-plugin-minify-html-literals').default,
+};
+
+const postcssPlugins = {
+	calc: require('postcss-calc'),
+	cssnano: require('cssnano'),
+    fontFamilySystemUi: require('postcss-font-family-system-ui'),
+	fontFamilySystemMonospace: require('postcss-system-monospace'),
+    import: require('postcss-import'),
+    nested: require('postcss-nested'),
 };
 
 gulp.task('rollup--worker', async () => {
@@ -65,6 +75,23 @@ gulp.task('eslint', () => {
 	);
 });
 
+gulp.task('css', () => {
+	return gulp.src('./src/**/*.css').pipe(
+		postcss([
+			postcssPlugins.import(),
+			postcssPlugins.nested(),
+			postcssPlugins.calc(),
+			postcssPlugins.fontFamilySystemUi(),
+			postcssPlugins.fontFamilySystemMonospace(),
+			postcssPlugins.cssnano({
+				cssDeclarationSorter: 'concentric-css',
+			}),
+		]),
+	).pipe(
+		gulp.dest('./dist/')
+	);
+});
+
 gulp.task('html', () => {
 	return gulp.src('./src/**/*.html').pipe(
 		newer('./dist/')
@@ -82,8 +109,9 @@ gulp.task('html', () => {
 
 gulp.task('default', gulp.series(...[
     'eslint',
-    'html',
     gulp.parallel(...[
+		'html',
+		'css',
         'rollup--init',
         'rollup--worker',
     ]),
